9c144689ea471d6dd512bed5444c8bc9bcce23dd
afgreen94 <afgreen94@gmail.com>
2025-10-05 11:13:45 -0400

    Added UpdateInterestsById command

---
+++ MyNotifier.Contracts/CommandAndControl/Commands/IUpdateInterestsById.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotifier.Contracts.CommandAndControl.Commands
+{
+    public interface IUpdateInterestsByIdDefinition : ICommandDefinition { }
+    public interface IUpdateInterestsById : ICommand { }
+}
