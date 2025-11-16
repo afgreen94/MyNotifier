bc8f89a2f2e433914ea5a0e679c557c70ef25aeb
afgreen94 <afgreen94@gmail.com>
2025-11-16 14:35:40 -0500

    Added Initializer

---
+++ MyNotifier.Contracts/Base/IInitializeable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotifier.Contracts.Base
+{
+    public interface IInitializeable
+    {
+        bool Initialized { get; }
+        int InitializerOrder { get; }
+
+        ValueTask<ICallResult> InitializeAsync(bool forceReinitialize = false);
+    }
+
+    public interface IInitializeable<TInitializerProperties>
+    {
+        bool Initialized { get; }
+        int InitializerOrder { get; }
+
+        ValueTask<ICallResult> InitializeAsync(TInitializerProperties initializerProperties, bool forceReinitialize = false);
+    }
+}
---
+++ MyNotifier/Base/Initializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyNotifier.Contracts.Base;
+
+namespace MyNotifier.Base
+{
+    public class Initializer
+    {
+        public static async ValueTask<ICallResult> InitializeAllAsync(IInitializeable[] initializeables, bool forceReinitialize = false, bool suppressOrdering = false)
+        {
+            try
+            {
+                if(!suppressOrdering) initializeables = initializeables.OrderBy(i => i.InitializerOrder).ToArray();
+
+                foreach (var initializeable in initializeables)
+                {
+                    if (!initializeable.Initialized || forceReinitialize)
+                    {
+                        var result = await initializeable.InitializeAsync(forceReinitialize).ConfigureAwait(false);
+
+                        if (!result.Success) { return CallResult.BuildFailedCallResult(result, $"Failed to initialize {initializeable.GetType().FullName}: {{0}}"); }
+                    }
+                }
+
+                return new CallResult();
+            }
+            catch (Exception ex) { return CallResult.FromException(ex);  }
+        }
+    }
+}
